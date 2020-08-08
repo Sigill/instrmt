aeb94eb43469ec0e3b1c364331bb346d0487d8d7
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2020-08-08 22:09:53 +0200

    Configure Travis CI

--- vendor/fetch_prerequisites.sh
+++ vendor/fetch_prerequisites.sh
@@ -202,9 +202,11 @@ while (( "$#" )); do
   shift
 done
 
-[ -n "$build_cmake2" ] && install_cmake2
-[ -n "$build_cmake3" ] && install_cmake3
-[ -n "$build_ittapi" ] && install_ittapi
-[ -n "$build_capstone" ] && install_capstone
-[ -n "$build_glfw" ] && install_glfw
-[ -n "$build_tracylib" -o -n "$build_tracycapture" -o -n "$build_tracyprofiler" ] && install_tracy
+[ -n "$build_cmake2" ] && { install_cmake2 || exit -1 ; }
+[ -n "$build_cmake3" ] && { install_cmake3 || exit -1 ; }
+[ -n "$build_ittapi" ] && { install_ittapi || exit -1 ; }
+[ -n "$build_capstone" ] && { install_capstone || exit -1 ; }
+[ -n "$build_glfw" ] && { install_glfw || exit -1 ; }
+[ -n "$build_tracylib" -o -n "$build_tracycapture" -o -n "$build_tracyprofiler" ] && { install_tracy || exit -1 ; }
+
+exit 0
