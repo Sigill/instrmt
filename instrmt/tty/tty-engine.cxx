8948e3f52c257c2919b2e0469e72945261d3feb6
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2020-09-05 17:15:47 +0200

    Remove unreleased C api for tty engine

--- instrmt/tty/tty-engine.cxx
+++ instrmt/tty/tty-engine.cxx
@@ -6,22 +6,6 @@
 
 #include <instrmt/tty/tty-utils.h>
 
-namespace {
-
-typedef struct {
-  const char* name;
-  int color;
-} TTYCRegionContext;
-
-TTYCRegionContext contexts[1000];
-
-typedef struct {
-  TTYCRegionContext* ctx;
-  double start;
-} TTYCRegion;
-
-} // anonymous namespace
-
 namespace instrmt {
 namespace tty {
 
@@ -86,33 +70,6 @@ extern "C" {
                                               int /*line*/)
 {
   return new instrmt::tty::RegionContext(name ? name : function);
-}
-
-void* make_c_region_context(const char* name,
-                            const char* function,
-                            const char* /*file*/,
-                            int /*line*/)
-{
-  static std::atomic<unsigned long> current_context(0);
-
-  TTYCRegionContext& ctx = contexts[current_context++];
-  ctx.name = name ? name : function;
-  ctx.color = instrmt_tty_string_color(ctx.name);
-
-  return &ctx;
-}
-
-void* begin_region(void* context) {
-  TTYCRegion* r = new TTYCRegion;
-  r->ctx = reinterpret_cast<TTYCRegionContext*>(context);
-  r->start = instrmt_get_time_ms();
-  return r;
-}
-
-void end_region(void* region) {
-  TTYCRegion* r = reinterpret_cast<TTYCRegion*>(region);
-  printf("\e[0;%dm%-40s \e[1;34m%.1f\e[0m ms\n", r->ctx->color, r->ctx->name,  instrmt_get_time_ms() - r->start);
-  delete r;
 }
 
 } // extern C
